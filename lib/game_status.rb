647929fb43cc34580a9d5763951c2cd9b1cfa604
Erin Volcan <erinvolcan@gmail.com>
2019-07-29 19:55:31 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,10 +16,10 @@ WIN_COMBINATIONS = [
 
 def won?(board)
   WIN_COMBINATIONS.each do |winner|
-    if winner == ["X", "X", "X"] || x == ["O", "O", "O"]
+    if winner == ["X", "X", "X"] || winner == ["O", "O", "O"]
       return winner 
     else
-      falsey 
+      false
     end 
   end
 end     
